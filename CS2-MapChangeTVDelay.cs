67b53d9ce945f21ddb287f231aac05163eb4aa56
a4594865-crypto <a4594865@gmail.com>
2026-05-13 17:20:27 +0800

    Update CS2-MapChangeTVDelay.cs

--- CS2-MapChangeTVDelay.cs
+++ CS2-MapChangeTVDelay.cs
@@ -54,7 +54,7 @@ public class OneVOneReset : BasePlugin
         if (player == null || !player.IsValid) return;
 
         // --- 發送私人武器選單訊息 ---
-        player.PrintToChat($" {ChatColors.Orange}輸 入 您 要 的 武 器，以 下 是 常 用 武 器");
+        player.PrintToChat($" {ChatColors.Orange}您 可 在 聊 天 欄 位 輸 入 您 要 的 武 器，以 下 是 常 用 武 器");
         player.PrintToChat($" -----------------------------------------------------------------");
         player.PrintToChat($" [ {ChatColors.Blue}手槍{ChatColors.White} ]  {ChatColors.Blue}!dg {ChatColors.White}[ 沙漠之鷹 ]   、 {ChatColors.Blue}!usp {ChatColors.White}[ USP-S ]   、 {ChatColors.Blue}!gk {ChatColors.White}[ 格洛克 ]");
         player.PrintToChat($" [ {ChatColors.Green}步槍{ChatColors.White} ] {ChatColors.Green}!ak {ChatColors.White}[ AK-47 ]   、 {ChatColors.Green}!a1 {ChatColors.White}[ M4-A1 ]   、 {ChatColors.Green}!a4 {ChatColors.White}[ M4-A4 ]");
